18a4c78e920827297cec2065c534a86f12debc61
William ALvarez <wramalvarez@gmail.com>
2016-05-31 08:50:39 -0500

    Accesos iniciales, dibujar el menu

---
+++ Scripts/01. accesosIniciales.sql
@@ -0,0 +1,18 @@
+insert into tmenu values ('1',null,'Administración','Administración',null,'SI',1,'NO',null,'ACT',1,null,'2016-01-01 23:50:59',null);
+insert into tmenu values('2','1','Seguridades','Seguridades del sistema',null,'SI',1,'NO','','ACT',1,null,'2016-01-01 23:50:59',null);
+insert into tmenu values('3','2','Administración de usuarios','Administración de usuarios','adminUsuarios','SI',1,'SI','usuariosP','ACT',1,null,'2016-01-01 23:50:59',null);
+insert into tmenu values('4','2','Administración de op1','Administración de op1','adminOp1','SI',2,'SI','usuariosP','ACT',1,null,'2016-01-06 23:50:59',null);
+insert into tmenu values('5','2','Administración de op2','Administración de op2','adminOp2','SI',3,'SI','usuariosP','ACT',1,null,'2016-01-11 23:50:59',null);
+insert into tmenu values('6','2','Administración de op3','Administración de op3','adminOp3','SI',4,'SI','usuariosP','ACT',1,null,'2016-01-11 23:50:59',null);
+insert into tmenu values('7','2','Administración de op4','Administración de op4','adminOp4','SI',5,'SI','usuariosP','ACT',1,null,'2016-01-11 23:50:59',null);
+insert into tmenu values('8','2','Administración de op6','Administración de op5','adminOp5','SI',5,'SI','usuariosP','ACT',1,null,'2016-01-11 23:50:59',null);
+
+--tmenus usuarios
+insert into tusuariomenu values(1,'1','ACT',1,null,'2016-05-28 23:50:59',null);
+insert into tusuariomenu values(1,'2','ACT',1,null,'2016-05-28 23:50:59',null);
+insert into tusuariomenu values(1,'3','ACT',1,null,'2016-05-28 23:50:59',null);
+insert into tusuariomenu values(1,'4','ACT',1,null,'2016-05-28 23:50:59',null);
+insert into tusuariomenu values(1,'5','ACT',1,null,'2016-05-28 23:50:59',null);
+insert into tusuariomenu values(1,'6','ACT',1,null,'2016-05-28 23:50:59',null);
+insert into tusuariomenu values(1,'7','ACT',1,null,'2016-05-28 23:50:59',null);
+
